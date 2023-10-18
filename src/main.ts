2625f6bbe43f0e77de89986963af9b68376ee6ca
Victor <linkgamesplays@gmail.com>
2023-10-18 11:55:48 -0300

    feat: change handling for main

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,17 @@
-import App, { type App as AppType } from './infra/app';
+import app from './infra/app';
 import Database, { type Database as DatabaseType } from './infra/db';
 
 export class Server {
-  private readonly app: AppType;
+  private readonly app;
   private readonly database: DatabaseType;
 
   constructor(){
-    this.app = App;
+    this.app = app;
     this.database = Database;
   }
 
   async initialize(): Promise<void> {
     await this.database.connect();
-
-    this.app.start();
-
-    process.on('SIGALRM', () => {
-      this.app.stop(async () => {
-        await this.database.disconnect();
-      })
-    })
   }
 }
 
