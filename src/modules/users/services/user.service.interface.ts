c3f876d50711022aa5750fb0b5ee287ffc94ed13
Victor <linkgamesplays@gmail.com>
2023-10-18 10:34:15 -0300

    feat:errors: create errors class

--- src/modules/users/services/user.service.interface.ts
+++ src/modules/users/services/user.service.interface.ts
@@ -2,6 +2,7 @@ import { type CreateUserDTO, type User } from '../dtos';
 
 export interface UserServiceInterface {
   create: (dto: CreateUserDTO) => Promise<User>;
-  findById: (id: string) => Promise<User>;
+  changeName: (id: string, newName: string) => Promise<User>
+  changePassword: (id: string, newPassword: string) => Promise<User>
   delete: (id: string) => Promise<User>;
 }
--- src/modules/users/services/user.service.ts
+++ src/modules/users/services/user.service.ts
@@ -1,15 +1,44 @@
-import { type UserServiceInterface } from "./user.service.interface";
-import { type UserRepositoryInterface } from "../repositories/user.repository.interface";
-import { type CreateUserDTO, type User } from "../dtos";
+import { type UserServiceInterface } from './user.service.interface';
+import { type UserRepositoryInterface } from '../repositories/user.repository.interface';
+import { type CreateUserDTO, type User } from '../dtos';
 
-export class UserService implements UserServiceInterface{
+export class UserService implements UserServiceInterface {
   constructor(private readonly userRepository: UserRepositoryInterface) {}
 
   async create(dto: CreateUserDTO): Promise<User> {
+    const emailAlreadyExists = await this.userRepository.findByEmail(dto.email);
+
+    if (emailAlreadyExists) throw new Error('Email already registered');
+
     const user = await this.userRepository.create(dto);
+    if (!user) throw new Error('Could not create');
 
-    if(!user) throw new Error("Could not create")
+    return user;
+  }
 
-    return user
+  async changeName(id: string, newName: string): Promise<User> {
+    const user = await this.userRepository.update(id, { name: newName });
+
+    if (!user) throw new Error('Could not find user');
+
+    return user;
+  }
+
+  async changePassword(id: string, newPassword: string): Promise<User> {
+    const user = await this.userRepository.update(id, {
+      password: newPassword,
+    });
+
+    if (!user) throw new Error('Could not find user');
+
+    return user;
+  }
+
+  async delete(id: string): Promise<User> {
+    const user = await this.userRepository.delete(id);
+
+    if (!user) throw new Error('Could not find user');
+
+    return user;
   }
 }
