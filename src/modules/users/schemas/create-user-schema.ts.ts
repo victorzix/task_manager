635d3fb1f12dab968591acd0830c61412bc0b857
Victor <linkgamesplays@gmail.com>
2023-10-20 09:50:39 -0300

    feat:users: create zod validation schema

---
+++ src/modules/users/schemas/create-user-schema.ts.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export const UserSchema = z.object({
+  name: z.string({ required_error: 'A name must be provided'}),
+  email: z.string({ required_error: 'An email must be provided'}).email('A valid email must be provided'),
+  password: z.string({ required_error: 'A password must be provided'}).min(8, 'Password must have at least 8 characters'),
+});
