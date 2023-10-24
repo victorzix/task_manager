8049709910ea89cd0cbec6b75d825f5f43ece342
victorzix <linkgamesplays@gmail.com>
2023-10-24 09:30:35 -0300

    fix:users: fix name validator string length

--- src/modules/users/schemas/create-user-schema.ts.ts
+++ src/modules/users/schemas/create-user-schema.ts.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const UserSchema = z.object({
-  name: z.string({ required_error: 'A name must be provided'}),
+  name: z.string({ required_error: 'A name must be provided'}).min(1, {message: "Name is required"}),
   email: z.string({ required_error: 'An email must be provided'}).email('A valid email must be provided'),
   password_hash: z.string({ required_error: 'A password must be provided'}).min(8, {message: 'Password must have at least 8 characters'}),
 });
