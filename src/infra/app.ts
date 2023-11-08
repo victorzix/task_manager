76deec308861798b8399421d5feef7bf695a8c19
victorzix <linkgamesplays@gmail.com>
2023-11-08 17:01:12 -0300

    refactor:infra: remove mongodb from usage

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import UserModule from '../modules/users/user.module';
+import AuthenticationModule from 'src/modules/auth/authentication.module';
 
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/user', UserModule.router)
+app.use('/', AuthenticationModule.router)
 
 
 
