457e337d336d7ea6daf4183cf8e3b37c1cdcbfea
victorzix <linkgamesplays@gmail.com>
2023-11-13 08:08:20 -0300

    feat:auth: create routes for auth controllers

---
+++ src/modules/auth/routes/authentication.route.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { AuthenticationControllerInterface } from '../controllers/authentication.controller.interface';
+import { checkAuth } from 'src/modules/middlewares/checkAuth';
+
+const router = Router();
+
+export class AuthenticationRouter {
+  public routes: Router[] = [];
+  constructor(
+    private readonly authenticationController: AuthenticationControllerInterface,
+  ) {
+    this.routes.push(
+      router.post(
+        '/login',
+        this.authenticationController.login.bind(authenticationController),
+      ),
+    );
+  }
+
+  public getRoutes(): Router[] {
+    return this.routes;
+  }
+}
