610e9424fa81adda153e54c3da8e67cd07d6dbb4
victorzix <linkgamesplays@gmail.com>
2023-11-13 08:07:31 -0300

    feat:utils: remove console.log from code

--- src/utils/password-hasher.ts
+++ src/utils/password-hasher.ts
@@ -3,7 +3,6 @@ import bcrypt from 'bcrypt';
 export default async function generateHashPassword(pass: string): Promise<string> {
   try {
     const hash = await bcrypt.hash(pass, 10);
-    console.log(hash);
     return hash;
   } catch (error) {
     throw error;
