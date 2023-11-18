ac554ddfa920a9f9fa2b6c1a8a138637ef76036e
victorzix <linkgamesplays@gmail.com>
2023-11-17 23:08:25 -0300

    refactor:users: change update request type

--- src/modules/users/routes/user.route.ts
+++ src/modules/users/routes/user.route.ts
@@ -7,8 +7,8 @@ export class UserRouter {
   public routes: Router[] = [];
   constructor(private readonly userController: UserControllerInterface) {
     this.routes.push(router.post('/', userController.createUser.bind(userController)))
-    this.routes.push(router.put('/password', userController.changePassword.bind(userController)))
-    this.routes.push(router.put('/name', userController.changeName.bind(userController)))
+    this.routes.push(router.patch('/password', userController.changePassword.bind(userController)))
+    this.routes.push(router.patch('/name', userController.changeName.bind(userController)))
     this.routes.push(router.delete('/', userController.deleteUser.bind(userController)))
   }
 
