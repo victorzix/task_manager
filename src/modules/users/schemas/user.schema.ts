f130bfab9d9fc143d3b1241f90e5e8b5638d6a15
victorzix <linkgamesplays@gmail.com>
2023-11-17 23:31:59 -0300

    feat:users: create update user schema

--- src/modules/users/schemas/user.schema.ts
+++ src/modules/users/schemas/user.schema.ts
@@ -1,7 +1,21 @@
 import { z } from 'zod';
 
 export const UserSchema = z.object({
-  name: z.string({ required_error: 'A name must be provided'}).min(1, {message: "Name is required"}),
-  email: z.string({ required_error: 'An email must be provided'}).email('A valid email must be provided'),
-  password: z.string({ required_error: 'A password must be provided'}).min(8, {message: 'Password must have at least 8 characters'}),
+  name: z
+    .string({ required_error: 'A name must be provided' })
+    .min(1, { message: 'Name is required' }),
+  email: z
+    .string({ required_error: 'An email must be provided' })
+    .email('A valid email must be provided'),
+  password: z
+    .string({ required_error: 'A password must be provided' })
+    .min(8, { message: 'Password must have at least 8 characters' }),
+});
+
+export const UpdateUserSchema = z.object({
+  name: z.string().optional(),
+  email: z.string().email('A valid email must be provided').optional(),
+  password: z
+    .string()
+    .min(8, { message: 'password must have at least 8 caracters' }),
 });
