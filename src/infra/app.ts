e483a9c009f6b5ccd527e11c41b9e4d82475e30b
victorzix <linkgamesplays@gmail.com>
2023-11-21 09:02:34 -0300

    chore:swagger: init swagger doc

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,15 +1,18 @@
 import express from 'express';
-import UserModule from '../modules/users/user.module';
-import AuthenticationModule from 'src/modules/auth/authentication.module';
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
+import AuthenticationModule from 'src/modules/auth/authentication.module';
+import UserModule from '../modules/users/user.module';
 import taskModule from 'src/modules/tasks/task.module';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from '../swagger.json'
 
 const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use('/', AuthenticationModule.router)
 app.use('/user', UserModule.router)
 app.use('/task', taskModule.router)
