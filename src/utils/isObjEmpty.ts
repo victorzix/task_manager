80b84bb2a812ab703742bbccec598d3914d82c32
victorzix <linkgamesplays@gmail.com>
2023-11-21 08:44:09 -0300

    feat:auth: create dtos for Token

--- src/utils/isObjEmpty.ts
+++ src/utils/isObjEmpty.ts
@@ -1,5 +1,3 @@
-import { BadRequestError } from 'src/errors/bad-request-error';
-
 export default function isObjEmpty<T extends Object>(obj: T): boolean {
   if (Object.keys(obj).length > 0) {
     return false;
