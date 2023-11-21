07467b9542ecfec0317701c7b96225689b182dde
victorzix <linkgamesplays@gmail.com>
2023-11-21 08:46:11 -0300

    feat:auth: add service in authmodule

--- src/modules/auth/authentication.module.ts
+++ src/modules/auth/authentication.module.ts
@@ -4,14 +4,18 @@ import { AuthenticationRouter } from './routes/authentication.route';
 import { AuthenticationControllerInterface } from './controllers/authentication.controller.interface';
 import { UserRepositoryInterface } from '../users/repositories/user.repository.interface';
 import { UserRepository } from '../users/repositories/user.repository';
+import { AuthenticationServiceInterface } from './services/authentication.service.interface';
+import { AuthenticationService } from './services/authentication.service';
 
 class AuthenticationModule {
   public controller: AuthenticationControllerInterface;
   public router: Router;
   public repository: UserRepositoryInterface
+  public service: AuthenticationServiceInterface
   constructor() {
     this.repository = new UserRepository();
-    this.controller = new AuthenticationController(this.repository);
+    this.service = new AuthenticationService(this.repository)
+    this.controller = new AuthenticationController(this.service);
     this.router = express.Router();
     this.router.use('/', new AuthenticationRouter(this.controller).getRoutes())
   }
