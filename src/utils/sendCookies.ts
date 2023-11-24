477156de3e95d10cab272ba341cb6dd17c7b6399
victorzix <linkgamesplays@gmail.com>
2023-11-24 14:02:16 -0300

    fix:auth: fixed secure error in cookie send

--- src/utils/sendCookies.ts
+++ src/utils/sendCookies.ts
@@ -2,18 +2,16 @@ import { Response } from 'express';
 
 export async function createAccessCookie(res: Response, payload: string): Promise<void> {
   res.cookie('access_token', payload, {
-    httpOnly: true,
     path: '/',
-    secure: true,
+    secure: false,
   });
   return;
 }
 
 export async function createRefreshCookie(res: Response, payload: string): Promise<void> {
   res.cookie('refresh_token', payload, {
-    httpOnly: true,
     path: '/',
-    secure: true,
+    secure: false,
   });
   return;
 }
